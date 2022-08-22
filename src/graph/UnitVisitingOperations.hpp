fcc2b0a9e389fbdd75a141167a81179bd6e46ebe
MrKubul <jakub.dryka1@gmail.com>
2022-08-22 11:52:38 +0200

    refactor node visiting system

---
+++ src/graph/UnitVisitingOperations.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+class UnitVisitingOperations
+{
+    private:
+        enum class VisitStatus{ Visited, Unvisited };
+        VisitStatus visitStatus = VisitStatus::Unvisited;
+    public:
+        bool get();
+        void setTrue();
+        void setFalse();
+};
+
+inline bool UnitVisitingOperations::get()
+{
+    return (this->visitStatus == VisitStatus::Visited ? true : false);
+}
+
+inline void UnitVisitingOperations::setTrue()
+{
+    visitStatus = VisitStatus::Visited;
+}
+
+inline void UnitVisitingOperations::setFalse()
+{
+    visitStatus = VisitStatus::Unvisited;
+}
